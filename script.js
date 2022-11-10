f20844a08a499a2c57a7dabf3f8409e41b51edd6
AlanRSilvaJW <alan.sithka@gmail.com>
2022-11-09 21:20:14 -0300

    condiçoes criados

---
+++ script.js
@@ -0,0 +1,46 @@
+
+/*const booleano1 = false
+const booleano2 = true
+const booleano3 = true
+
+if(booleano1){
+    alert('Entrou no if')
+}else{
+    alert('Entrou no else')
+}
+*/
+
+/*
+if(booleano1===booleano2){
+    alert('O valor das variáveis 1 e 2 são iguais')
+}else if(booleano2===booleano3){
+     alert('O valor das variáveis 2 e 3 são iguais')
+}else if(booleano1===booleano3){
+    alert('O valor das variáveis 1 e 3 sãoi iguais')
+}else
+    alert('Não existem valores iguais')
+    */
+
+    /*
+    const idade = 10
+
+    if(idade >= 60 || idade >= 16 && idade < 18){
+        console.log('Voto Facultativo')
+    }else if(idade >= 18){
+        console.log('Tem direito ao título de eleitor')
+    }else{
+        console.log('Você não tem idade mínima para emissaõ do título de eleitor')
+    }
+    */
+
+    const media = 5
+
+    if (media >= 5){
+        console.log('Aluno foi Aprovado')
+        }else if(media >=3){
+            console.log('Aluno em Recuperação')
+        }else if(media < 3){
+            console.log('Aluno Reprovado')
+        }else{
+            console.log('Nada consta nos dados desse Aluno')
+        }
